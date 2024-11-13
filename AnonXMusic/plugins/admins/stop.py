e4978384211bb634968e4f93fa57009ac9a9e3ad
ehsan1374694 <83925684+ehsan1374694@users.noreply.github.com>
2024-11-13 11:05:23 +0300

    Update stop.py
    Signed-off-by: ehsan1374694 <83925684+ehsan1374694@users.noreply.github.com>

--- AnonXMusic/plugins/admins/stop.py
+++ AnonXMusic/plugins/admins/stop.py
@@ -10,13 +10,13 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["اتمام", "پایان", "cend", "cstop"],prefixes=['']) & filters.group & ~BANNED_USERS
+    filters.command(["پایان", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await Onix.stop_stream(chat_id)
+    await Anony.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
