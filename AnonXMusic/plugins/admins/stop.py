eb0382df610cfd5d80e21d6f20fa39e1cd62b641
ehsan1374694 <83925684+ehsan1374694@users.noreply.github.com>
2024-11-13 10:38:28 +0300

    Update stop.py
    Signed-off-by: ehsan1374694 <83925684+ehsan1374694@users.noreply.github.com>

--- AnonXMusic/plugins/admins/stop.py
+++ AnonXMusic/plugins/admins/stop.py
@@ -9,15 +9,28 @@ from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
+
 @app.on_message(
-    filters.command(["end", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
+    filters.command(["end", "اتمام", "cend", "cstop"],prefixes=['']) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await Anony.stop_stream(chat_id)
+    await Onix.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
+
+@app.on_message(
+    filters.command(["شارژ"],prefixes=['']) & filters.group & ~BANNED_USERS
+)
+async def group_charge(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    month = message.command[1]
+    print('month',month)
+    await message.reply_text(
+        f"گروه با موفقیت {month} روز شارژ شد ."
     )
